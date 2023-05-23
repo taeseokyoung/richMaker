bc3f5594b8b91d87a921d0201cdc236453e3ca4f
SungSuSon <thstjdtn05@gmail.com>
2023-05-23 20:31:14 +0900

    :bug:Fix: update users app views.py
    병합 과정에서 생긴 문제 수정

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Challenge, Accountminus, Accountplus, Income, ConsumeStyle
+from .models import Challenge, Accountminus, Accountplus, Income, ConsumeStyle,Comment
 # Register your models here.
 admin.site.register(Challenge)
 admin.site.register(Accountminus)
 admin.site.register(Accountplus)
 admin.site.register(Income)
 admin.site.register(ConsumeStyle)
+admin.site.register(Comment)
